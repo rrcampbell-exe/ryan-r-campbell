9d4bf01ea1c7660ecf5f3c5e2791597b022ca13e
rrcampbell-exe <campbell.ryan.r@gmail.com>
2024-04-07 10:04:20 -0500

    feat: introduce landing page, generic about pages

---
+++ src/experiences/author/pages/Landing/AuthorLanding.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const AuthorLanding = () => {
+  return (
+    <div>
+      <header className='page-banner'>
+        <h1>ryan r. campbell</h1>
+        <h2>two-time international book awards finalist</h2>
+      </header>
+      <main className='section-wrapper'>
+        some content
+      </main>
+      <footer>
+        hello
+      </footer>
+    </div>
+  )
+}
+
+export default AuthorLanding
---
+++ src/experiences/shared/pages/About/GeneralAbout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const GeneralAbout = () => {
+  return (
+    <div>
+      <header className='page-banner'>
+        <h1>ryan r. campbell</h1>
+        <h2>about</h2>
+      </header>
+    </div>
+  )
+}
+
+export default GeneralAbout
