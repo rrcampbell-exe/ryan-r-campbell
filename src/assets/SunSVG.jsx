8162fd4ec13d81a38eec4b0d673e4b6f2b29d79c
rrcampbell-exe <campbell.ryan.r@gmail.com>
2024-04-10 15:24:06 -0500

    fix: update stroke width

--- src/assets/SunSVG.jsx
+++ src/assets/SunSVG.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 const SunSVG = () => {
   return (
     <svg className="banner-svg" id="sun-svg" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <circle cx="12" cy="12" r="6" stroke-width="2.5"/>
-      <path d="M12 2V3" stroke-width="2.5" stroke-linecap="round"/>
-      <path d="M12 21V22" stroke-width="2.5" stroke-linecap="round"/>
-      <path d="M22 12L21 12" stroke-width="2.5" stroke-linecap="round"/>
-      <path d="M3 12L2 12" stroke-width="2.5" stroke-linecap="round"/>
-      <path d="M19.0708 4.92969L18.678 5.32252" stroke-width="2.5" stroke-linecap="round"/>
-      <path d="M5.32178 18.6777L4.92894 19.0706" stroke-width="2.5" stroke-linecap="round"/>
-      <path d="M19.0708 19.0703L18.678 18.6775" stroke-width="2.5" stroke-linecap="round"/>
-      <path d="M5.32178 5.32227L4.92894 4.92943" stroke-width="2.5" stroke-linecap="round"/>
+      <circle cx="12" cy="12" r="6" stroke-width="2"/>
+      <path d="M12 2V3" stroke-width="2" stroke-linecap="round"/>
+      <path d="M12 21V22" stroke-width="2" stroke-linecap="round"/>
+      <path d="M22 12L21 12" stroke-width="2" stroke-linecap="round"/>
+      <path d="M3 12L2 12" stroke-width="2" stroke-linecap="round"/>
+      <path d="M19.0708 4.92969L18.678 5.32252" stroke-width="2" stroke-linecap="round"/>
+      <path d="M5.32178 18.6777L4.92894 19.0706" stroke-width="2" stroke-linecap="round"/>
+      <path d="M19.0708 19.0703L18.678 18.6775" stroke-width="2" stroke-linecap="round"/>
+      <path d="M5.32178 5.32227L4.92894 4.92943" stroke-width="2" stroke-linecap="round"/>
       </svg>
   )
 }
