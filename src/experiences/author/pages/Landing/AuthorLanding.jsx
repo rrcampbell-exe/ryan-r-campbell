1328a506ee487a66542800ca5f94246f0da6c0a3
rrcampbell-exe <campbell.ryan.r@gmail.com>
2024-04-10 15:18:10 -0500

    feat: add new content

--- src/experiences/author/pages/Landing/AuthorLanding.jsx
+++ src/experiences/author/pages/Landing/AuthorLanding.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import PageWrapper from '../../../shared/components/PageWrapper/PageWrapper'
+import { Link } from 'react-router-dom'
 
 const AuthorLanding = () => {
   return (
     <PageWrapper pageTitle='two-time international book awards finalist'>
       <main className='section-wrapper'>
-        some content
+        <div className='about-row'>
+          <div className='about-col'>
+            <img className='author-headshot' src='./2024-ryan-r-campbell-website-headshot.png' />
+          </div>
+          <div className='about-col'>
+            <h2>about</h2>
+            <p>
+              Ryan R. Campbell is a two-time International Book Awards finalist, the founder of the Writescast Network, and the co-founder of Kill Your Darlings Candle Company. 
+            </p>
+            <Link to='/author/about'>learn more</Link>
+          </div>
+        </div>
+        <h2>books</h2>
+        <h2>follow</h2>
       </main>
     </PageWrapper>
   )
