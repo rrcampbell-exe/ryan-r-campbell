327d6e0f8e1899b98c3968e5a66c4a5693b39ca7
rrcampbell-exe <campbell.ryan.r@gmail.com>
2024-04-12 13:52:31 -0500

    feat: introduce pop text in page banner

---
+++ src/constants/pop-text.js
@@ -0,0 +1,31 @@
+const popText = [
+  {
+    text: 'author'
+  },
+  {
+    text: 'technologist'
+  },
+  {
+    text: 'software engineer'
+  },
+  {
+    text: 'linguist'
+  },
+  {
+    text: 'stargazer'
+  },
+  {
+    text: 'dog dad'
+  },
+  {
+    text: 'traveler'
+  },
+  {
+    text: 'cat dad'
+  },
+  {
+    text: 'polyglot'
+  }
+]
+
+export default popText
