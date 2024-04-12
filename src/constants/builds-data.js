61de1ed3bc1fd29c8bb51a9b4d53db53b0f29e82
rrcampbell-exe <campbell.ryan.r@gmail.com>
2024-04-12 13:52:09 -0500

    feat: add app constants

---
+++ src/constants/builds-data.js
@@ -0,0 +1,34 @@
+const builds = [
+  {
+    logo: './builds/real-fake-birds-app-logo.png',
+    alt: 'The cartoon outline of a bird against a forest green background',
+    title: 'Real Fake Birds',
+    teaser: 'Some are real. Some are fake. Tell them apart.',
+    technologies: 'React, Node, axios, styled-components, react-router-dom, REST APIs',
+    link: 'https://www.realfakebirds.app',
+    linkText: 'play the game',
+    gitHub: 'https://github.com/rrcampbell-exe/real-fake-birds'
+  },
+  {
+    logo: './builds/yarnsworths-tales-app-logo.png',
+    alt: 'The cartoon cat Yarnsworth grins mischieviously',
+    title: 'Yarnsworth\'s Tales',
+    teaser: 'Trillions of tales await.',
+    technologies: 'HTML, JavaScript, CSS, Bootstrap',
+    link: 'https://rrcampbell-exe.github.io/yarnsworths-tales/',
+    linkText: 'discover your next d&d adventure',
+    gitHub: 'https://github.com/rrcampbell-exe/yarnsworths-tales'
+  },
+  {
+    logo: './builds/ryan-r-campbell-app-logo.png',
+    alt: 'A black-and-white image of Ryan R. Campbell gazing to the right',
+    title: 'The Ryan R. Campbell Website',
+    teaser: 'Books and builds and blogs, oh my!',
+    technologies: 'React, Sass, Vite, react-router-dom',
+    link: 'https://ryanrcampbell.com/',
+    linkText: 'return to the homepage',
+    gitHub: 'https://github.com/rrcampbell-exe/ryan-r-campbell'
+  },
+]
+
+export default builds
