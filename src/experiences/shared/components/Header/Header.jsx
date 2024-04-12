cd7dceef3753bb0a53dfb37aa4d5b2339a551aa0
rrcampbell-exe <campbell.ryan.r@gmail.com>
2024-04-12 13:53:01 -0500

    fix: remove unnecessary import

--- src/experiences/shared/components/Header/Header.jsx
+++ src/experiences/shared/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext } from 'react'
 import MoonSVG from '../../../../assets/MoonSVG'
 import SunSVG from '../../../../assets/SunSVG'
 import { ThemeContext } from '../../../../Theme'
