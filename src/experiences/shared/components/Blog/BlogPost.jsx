2c82e6a38b8bd5d3e962aa6ace184c250e1e0bf3
rrcampbell-exe <campbell.ryan.r@gmail.com>
2024-04-14 13:27:19 -0500

    feat: introduce legacy blog posts

--- src/experiences/shared/components/Blog/BlogPost.jsx
+++ src/experiences/shared/components/Blog/BlogPost.jsx
@@ -1,17 +1,52 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import posts from '../../../../assets/posts/posts'
+import { useParams } from 'react-router-dom'
+import fetchPost from '../../../../utils/fetch-post'
+import extractHTMLContent from '../../../../utils/extract-html'
+import PageWrapper from '../PageWrapper/PageWrapper'
+import formatDate from '../../../../utils/format-date'
+import shortBio from '../../../../constants/short-bio'
+
+// TODO: need to insert category tags (if possible)
+// TODO: need to sanitize remaining HTML of comments related to divi
+// TODO: need to remove footer contents with book recommendation at the end
 
 const BlogPost = () => {
-  const [postContent, setPostContent] = useState('')
+  const { year, month, day, slug } = useParams()
 
-  console.log('these are the posts', posts.slice(0, 3))
+  const post = fetchPost(year, month, day, slug, posts)
 
-  console.log('this is the content of the first post', posts[0].content.rendered)
+  const { title: { rendered: title }, date, episode_featured_image, content: { rendered: contentToRender } } = post
 
-  const firstPostContent = posts[0].content.rendered
+  const HTMLcontent = extractHTMLContent(contentToRender)
 
   return (
-    <div dangerouslySetInnerHTML={{ __html: firstPostContent }}/>
+    <PageWrapper pageTitle='author | technologist'>
+      <main>
+        <div className='BlogPost'>
+          <div className='masthead'>
+            <img className='cover-image' src={episode_featured_image} />
+            <div>
+              <h2 className='title'>{title}</h2>
+              <div className='credits'>
+                <div className='byline-container'>
+                  <div className='author-photo'>
+                    <img src='/2024-ryan-r-campbell-website-headshot.png' />
+                  </div>
+                  <div>
+                    <p>Written by Ryan R. Campbell</p> 
+                    <p className='posted-on-date'>Posted on {formatDate(date)}</p>
+                  </div>
+                </div>
+                <p className='bio'>{shortBio}</p>
+                <div className='blog-post-underline' />
+              </div>
+            </div>
+          </div>
+          <div dangerouslySetInnerHTML={{ __html: HTMLcontent }}/>
+        </div>
+      </main>
+    </PageWrapper>
   )
 }
 
--- src/experiences/shared/components/Blog/BlogTeaser.jsx
+++ src/experiences/shared/components/Blog/BlogTeaser.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import posts from '../../../../assets/posts/posts'
 import formatDate from '../../../../utils/format-date'
+import { Link } from 'react-router-dom'
+import buildBlogParams from '../../../../utils/build-blog-params'
 
 const BlogTeaser = () => {
   const teaserPosts = posts.slice(0, 3)
 
-  console.log('these are the teaserPosts', teaserPosts)
-
   return (
-    <div>
+    <div className='BlogTeaser'>
       {teaserPosts.map((post) => (
-        <>
+        <div className='blog-post grow-on-hover'>
           <h3>{post.title.rendered}</h3>
           <span>Posted on {formatDate(post.date)}</span>
           <p>{post.excerpt.rendered}</p>
-          <a href={post.link} target='_blank'>read more</a>
-        </>
+          <Link to={buildBlogParams(post.link)}>read more</Link>
+          <div className='blog-post-underline' />
+        </div>
       ))}
     </div>
   )
