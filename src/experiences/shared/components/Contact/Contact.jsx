1a06980bd92cdf52790b95640405e4f36cf3c825
rrcampbell-exe <campbell.ryan.r@gmail.com>
2024-04-14 13:27:40 -0500

    feat: introduce contact section

---
+++ src/experiences/shared/components/Contact/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className='Contact'>
+      <h4>tech inquiries</h4>
+      <p>campbell [dot] ryan [dot] r [at] gmail [dot] com</p>
+      <h4>author inquiries</h4>
+      <p>rrcampbellwrites [at] gmail [dot] com</p>
+    </div>
+  )
+}
+
+export default Contact
