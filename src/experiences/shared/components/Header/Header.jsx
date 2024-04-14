81327db84bed43137e6fd572ea8bb32e8c6afe08
rrcampbell-exe <campbell.ryan.r@gmail.com>
2024-04-14 13:27:54 -0500

    feat: add link to header

--- src/experiences/shared/components/Header/Header.jsx
+++ src/experiences/shared/components/Header/Header.jsx
@@ -2,15 +2,18 @@ import React, { useContext } from 'react'
 import MoonSVG from '../../../../assets/MoonSVG'
 import SunSVG from '../../../../assets/SunSVG'
 import { ThemeContext } from '../../../../Theme'
+import { Link } from 'react-router-dom'
 
 const Header = ({ pageTitle }) => {
   const { theme, toggleTheme } = useContext(ThemeContext)
   return (
-    <header className='page-header'>
-      <div>
-        <h1>ryan r<span id='header-dot'>.</span> campbell</h1>
-        <h2>{pageTitle}</h2>
-      </div>
+    <header className='Header'>
+      <Link to='/' className='link'>
+        <div>
+          <h1>ryan r<span id='header-dot'>.</span> campbell</h1>
+          <h2>{pageTitle}</h2>
+        </div>
+      </Link>
       <div>
         <button className='theme-toggle-button' onClick={() => toggleTheme()}>
           {theme === 'light-theme' ? <SunSVG /> : <MoonSVG />}
