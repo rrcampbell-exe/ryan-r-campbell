3dbebf383dc7f6c622b046c2fad8d6647ec4e7a6
rrcampbell-exe <campbell.ryan.r@gmail.com>
2024-04-16 10:36:18 -0500

    feat: robustify constants

---
+++ src/constants/awards.js
@@ -0,0 +1,25 @@
+const awards = [
+  { 
+    name: '2022 International Book Awards Finalist - Humor and Satire',
+    alt: "A wreath containing the words International Book Awards Finalist - 2022 | Humor & Satire",
+    link: 'https://www.internationalbookawards.com/2022pressrelease.html',
+    src: './awards/2022-international-book-awards-finalist-humor-and-satire.png',
+    className: 'light award',
+  },
+  {
+    name: "Readers' Favorite Five Stars",
+    alt: "The logo for the Readers' Favorite Five Stars award, which is a silver medallion that features five stars",
+    link: 'https://readersfavorite.com/book-review/scambait',
+    src: './awards/readers-favorite-five-stars.png',
+    className: 'award',
+  },
+  {
+    name: '2020 International Book Awards Finalist - Science Fiction',
+    alt: "A wreath containing the words International Book Awards Finalist - 2020 | Science Fiction",
+    link: 'https://ryanrcampbell.com/2020/07/23/imminent-dawn-named-as-finalist-in-2020-international-book-awards/',
+    src: './awards/2020-international-book-awards-finalist-science-fiction.png',
+    className: 'light award',
+  }
+]
+
+export default awards
