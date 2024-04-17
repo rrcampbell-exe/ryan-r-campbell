a0bedb49448474975d6a7601c110b585e9cffbe8
rrcampbell-exe <campbell.ryan.r@gmail.com>
2024-04-17 12:57:37 -0500

    fix: make full book clickable

--- src/experiences/shared/components/Books/Books.jsx
+++ src/experiences/shared/components/Books/Books.jsx
@@ -6,12 +6,14 @@ const Books = () => {
   return (
     <div className='Books'>
       {books.map((book) => (
-        <div className='book grow-on-hover'>
-          <img className='book-cover' src={book.cover} alt={book.alt} />
-          <h3 className='book-title'>{book.title}</h3>
-          <p>{book.summary}</p>
-          <Link to={'/books'}>read more</Link>
-        </div>
+        <Link to={'/books'}>
+          <div className='book grow-on-hover'>
+            <img className='book-cover' src={book.cover} alt={book.alt} />
+            <h3 className='book-title'>{book.title}</h3>
+            <p>{book.summary}</p>
+            <Link to={'/books'}>read more</Link>
+          </div>
+        </Link>
       ))}
     </div>
   )
