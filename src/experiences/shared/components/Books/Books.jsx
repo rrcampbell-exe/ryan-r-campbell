16da7612e2a67fe40b3a79dd6e209f9037526f68
rrcampbell-exe <campbell.ryan.r@gmail.com>
2024-04-17 16:48:11 -0500

    refactor: implement useNavigate instead of Link

--- src/experiences/shared/components/Books/Books.jsx
+++ src/experiences/shared/components/Books/Books.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import { books } from '../../../../constants'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Books = () => {
+  const navigate = useNavigate()
   return (
     <div className='Books'>
       {books.map((book) => (
-        <Link to={'/books'}>
+        <div onClick={() => navigate('/books')}>
           <div className='book grow-on-hover'>
             <img className='book-cover' src={book.cover} alt={book.alt} />
             <h3 className='book-title'>{book.title}</h3>
             <p>{book.summary}</p>
             <Link to={'/books'}>read more</Link>
           </div>
-        </Link>
+        </div>
       ))}
     </div>
   )
