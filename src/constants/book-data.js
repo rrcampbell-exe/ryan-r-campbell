67d4baa1daa7c0b8eb14dcef28b9ba7e85cd737b
rrcampbell-exe <campbell.ryan.r@gmail.com>
2024-04-17 07:03:24 -0500

    feat: robustify books data

--- src/constants/book-data.js
+++ src/constants/book-data.js
@@ -1,31 +1,63 @@
 const books = [
   {
     title: 'Scambait',
-    cover: './books/scambait-flat-cover-international-book-awards-readers-favorite-sticker.png',
+    genre: 'Contemporary Fiction | Humor',
+    cover: './books/scambait/scambait-flat-cover-international-book-awards-readers-favorite-sticker.png',
     alt: 'Orange cover of the book Scambait by Ryan R. Campbell, which displays the book title and shows the main character sitting at a desk with a computer and his Saint Bernard at his side',
     summary: 'The International Book Awards finalist in Humor and Satire that asks, "Who scams the scammers?"',
-    readMoreLink: 'https://www.wikipedia.org/2024'
+    fullDescription: {
+      paragraphOne: 'Ever since his grandma squandered the family fortune to two-bit hucksters from “Microsoft Tech Support,” corporate do-little Eric Amundsen has spent his workdays wasting the time of Nigerian princes and car warranty salesmen everywhere.',
+      paragraphTwo: `But when Eric's supposedly long-deceased father contacts him through his spam folder, Eric—whose workday mantra isn't exactly “solutions-oriented”—has serious problems to solve.`
+    },
+    readMoreLink: './books/scambait/scambait-teaser-chapters.pdf',
+    buyLink: 'https://www.amazon.com/dp/B09SY4P6FR',
+    shortPromoDescription: 'The 2022 International Book Awards Finalist in Humor and Satire.',
+    id: 'scambait'
   },
   {
     title: 'And Ampersand',
-    cover: './books/and-ampersand-flat-cover.png',
+    genre: 'Works of Short Fiction',
+    cover: './books/and-ampersand/and-ampersand-flat-cover.png',
     alt: 'The black cover of the book And Ampersand by Ryan R. Campbell, which features two large ampersands in gold and white, respectively',
     summary: 'A collection of short fiction that begins with endings, ends with beginnings, and elevates feel over form.',
-    readMoreLink: 'https://www.wikipedia.org/2024'
+    fullDescription: {
+      paragraphOne: 'Your death comes on slowly, while elsewhere, a nameless man fakes his own. Encased in glass, a young man navigates a world meant for those beyond. A mudslide washes away a once romantic weekend, and a childhood swim drowns all illusion of innocence.',
+      paragraphTwo: `These are but some of the tales in this collection of flash fiction, short stories, and freeform prose.`
+    },
+    readMoreLink: './books/and-ampersand/glassed-excerpt-from-and-ampersand.pdf',
+    buyLink: 'https://www.amazon.com/dp/1736387111',
+    shortPromoDescription: "The perfect pocket companion for any reader who's tussled with the tie between beginning and end.",
+    id: 'and-ampersand'
   },
   {
     title: 'Imminent Dawn',
-    cover: './books/imminent-dawn-flat-cover-international-book-awards-sticker.png',
+    genre: 'Science Fiction',
+    cover: './books/imminent-dawn/imminent-dawn-flat-cover-international-book-awards-sticker.png',
     alt: 'The blue cover of the book Imminent Dawn by Ryan R. Campbell, which shows the silhouette of a woman against a backdrop of stars and circuitry',
     summary: 'The International Book Awards finalist and first installment in the EMPATHY sci-fi saga.',
-    readMoreLink: 'https://www.wikipedia.org/2024'
+    fullDescription: {
+      paragraphOne: 'When an art-school dropout enrolls in the first round of human trials for an internet-access brain implant, she never expects to find herself at the center of conspiracies stretching from the highest levels of the North American Union government to the lowest dredges of its shadowy hacking collectives.',
+      paragraphTwo: ''
+    },
+    readMoreLink: './books/imminent-dawn/imminent-dawn-sample-chapter.pdf',
+    buyLink: 'https://www.amazon.com/dp/B07N1Q8C6G',
+    shortPromoDescription: 'The International Book Awards finalist and first installment in the EMPATHY sci-fi saga.',
+    id: 'imminent-dawn'
   },
   {
     title: 'Mourning Dove',
-    cover: './books/mourning-dove-flat-cover.png',
+    genre: 'Science Fiction',
+    cover: './books/mourning-dove/mourning-dove-flat-cover.png',
     alt: 'The golden cover of the book Mourning Dove by Ryan R. Campbell, which features the silhouettes of a woman with a backpack and a cat set against a technothriller-like web of connected dots',
     summary: 'An evocative, sweeping symphony of love, revenge, and desperation in cacophonous times.',
-    readMoreLink: 'https://www.wikipedia.org/2024'
+    fullDescription: {
+      paragraphOne: 'In the aftermath of the Human/Etech research study, spouse is further pit against spouse, brother against sister, and governments against their people. In the end, all must choose between reclaiming the past or surrendering to an intractable world of their own creation.',
+      paragraphTwo: ''
+    },
+    readMoreLink: './books/mourning-dove/mourning-dove-sample-chapter.pdf',
+    buyLink: 'https://www.amazon.com/dp/B07QZGSDX6',
+    shortPromoDescription: 'The sequel to International Book Awards finalist, Imminent Dawn.',
+    id: 'mourning-dove'
   },
 ]
 
