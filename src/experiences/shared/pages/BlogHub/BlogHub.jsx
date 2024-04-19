8d5541b3e142b9567c169fcba6bf973fb71c25fb
rrcampbell-exe <campbell.ryan.r@gmail.com>
2024-04-19 16:45:50 -0500

    fix: disable button to load more posts once all posts loaded

--- src/experiences/shared/pages/BlogHub/BlogHub.jsx
+++ src/experiences/shared/pages/BlogHub/BlogHub.jsx
@@ -1,15 +1,23 @@
 import React, { useState } from 'react'
 import { PageWrapper, BlogTeaser, Contact } from '../../components'
+import posts from '../../../../assets/posts/posts'
 
 const BlogHub = () => {
   const [postsPage, setPostsPage] = useState(1)
 
+  const totalPostsQty = posts.length
+  const postsToDisplay = 9
+  const numberOfPostsShown = postsToDisplay * postsPage
+  const allBlogPostsShown = numberOfPostsShown >= totalPostsQty
+
+  const buttonText = allBlogPostsShown ? 'all blog posts shown' : 'load more posts'
+
   return (
     <PageWrapper pageTitle='blogger'>
       <main className='BlogHub'>
-        <BlogTeaser postsToDisplay={9} showCoverImage postsPage={postsPage} />
+        <BlogTeaser postsToDisplay={postsToDisplay} showCoverImage postsPage={postsPage} />
         <div className='blog-hub-nav'>
-          <button onClick={() => setPostsPage(postsPage + 1)}>load more posts</button>
+          <button onClick={() => setPostsPage(postsPage + 1)} disabled={allBlogPostsShown}>{buttonText}</button>
         </div>
         <div id='contact' className='section-wrapper shadow-bg-main-accent'>
           <h2>contact</h2>
