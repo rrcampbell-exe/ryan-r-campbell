0ac532256a2b675cd2349731e94aa512c86e137f
rrcampbell-exe <campbell.ryan.r@gmail.com>
2024-04-20 08:15:05 -0500

    feat: introduce unstyled hamburger menu

---
+++ src/experiences/shared/components/HamburgerMenu/HamburgerMenu.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import HamburgerIcon from '../../../../assets/HamburgerIcon'
+
+// TODO: need icon to update on menu open
+// TODO: styles for menu, including animation
+// TODO: ensure all pages accounted for in menu
+
+const HamburgerMenu = () => {
+  const [menuIsOpen, setMenuIsOpen] = useState(false)
+  const navigate = useNavigate()
+  return (
+    <div className='Hamburger-menu'>
+      <button onClick={() => setMenuIsOpen(!menuIsOpen)}>
+        <HamburgerIcon />
+      </button>
+      {menuIsOpen &&
+        <>
+          <div onClick={() => navigate('/about')}>about</div>
+          <div onClick={() => navigate('/author')}>author</div>
+          <div onClick={() => navigate('/books')}>books</div>
+          <div onClick={() => navigate('/technologist')}>technologist</div>
+          <div onClick={() => navigate('/blog')}>blog</div>
+          <div onClick={() => navigate('/podcasts')}>podcasts</div>
+        </>
+      }
+    </div>
+  )
+}
+
+export default HamburgerMenu
+
