3d2b0a225da14a66dbf038fdf838906a3ba089a5
rrcampbell-exe <campbell.ryan.r@gmail.com>
2024-04-20 08:15:29 -0500

    fix: update styles

--- src/assets/SunSVG.jsx
+++ src/assets/SunSVG.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SunSVG = () => {
   return (
-    <svg className="banner-svg" id="sun-svg" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg className="header-svg" id="sun-svg" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
       <circle cx="12" cy="12" r="6" strokeWidth="2"/>
       <path d="M12 2V3" strokeWidth="2" strokeLinecap="round"/>
       <path d="M12 21V22" strokeWidth="2" strokeLinecap="round"/>
