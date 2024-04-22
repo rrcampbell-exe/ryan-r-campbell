3a3e3ae04c32c4f7573aa039322481c4afbd7ab1
rrcampbell-exe <campbell.ryan.r@gmail.com>
2024-04-22 17:10:01 -0500

    feat: introduce about banner, subnav, styles for same

--- src/experiences/author/pages/Podcast/PodcastLanding.jsx
+++ src/experiences/author/pages/Podcast/PodcastLanding.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react'
-import { PageWrapper, Contact } from '../../../shared/components'
+import { PageWrapper, Contact, SubNav } from '../../../shared/components'
 import PodcastTeaser from './PodcastTeaser'
 import podcasts from '../../../../assets/podcasts/pinecast-podcasts'
-
 
 const PodcastLanding = () => {
   const [episodesPage, setEpisodesPage] = useState(1)
@@ -16,16 +15,23 @@ const PodcastLanding = () => {
 
   return (
     <PageWrapper pageTitle='podcaster | community-builder'>
+      <SubNav sections={['about', 'podcast-player', 'all-episodes']} />
       <main className='Podcast'>
-        <h2>the writescast network</h2>
-        <p>Founded in 2016, the Writescast Network reaches tens of thousands of writers and creatives around the globe. It may have ceased broadcasting new episodes in December of 2020, but it lives on here for streaming any time, anywhere.</p>
-        <div className='podcast-player'>
-          <iframe src='https://pinecast.com/embed/player_playlist/the-writescast-network?color.primary=%23000000&amp;color.secondary=%23cbd3da&amp;color.accent=%23df633b' border='0' style={{ border: '0', borderRadius: '4px' }} height='600'></iframe>
+        <div id='about' className='about-row section-wrapper shadow-bg-gray'>
+          <div>
+            <img className='author-headshot' src='./podcasts/writescast-network-circle-logo.png' />
+          </div>
+          <div className='section-wrapper about-description'>
+            <h2>about</h2>
+            <p>Founded in 2016, the Writescast Network reaches tens of thousands of writers and creatives around the globe. It may have ceased broadcasting new episodes in December of 2020, but it lives on here for streaming any time, anywhere.</p>
+          </div>
         </div>
-        <div className='BlogHub'>
-          <h2>episode pages</h2>
+        <div id='podcast-player' className='podcast-player'>
+          <iframe src='https://pinecast.com/embed/player_playlist/the-writescast-network?color.primary=%23000000&amp;color.secondary=%23cbd3da&amp;color.accent=%23df633b' border='0' style={{ border: '0', borderRadius: '4px' }} height='400'></iframe>
+        </div>
+        <div id='all-episodes' className='ContentHub'>
           <PodcastTeaser episodesToDisplay={episodesToDisplay} episodesPage={episodesPage}/>
-          <div className='blog-hub-nav'>
+          <div className='content-hub-nav'>
             <button onClick={() => setEpisodesPage(episodesPage + 1)} disabled={allEpisodesShown}>{buttonText}</button>
           </div>
         </div>
