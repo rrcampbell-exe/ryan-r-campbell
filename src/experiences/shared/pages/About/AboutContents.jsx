c3dd22106c4641108ae8c7bd9d477b64cafdc2a7
rrcampbell-exe <campbell.ryan.r@gmail.com>
2024-04-26 15:22:08 -0500

    feat: begin to write about page

---
+++ src/experiences/shared/pages/About/AboutContents.jsx
@@ -0,0 +1,96 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export const ShortStory = () =>
+  <>
+    <h2>the tl;dr</h2>
+    <div id='writerly-wrangling'>
+      <h3>writerly wrangling</h3>
+      <p>I'm a two-time International Book Awards finalist, the founder of the Writescast Network, and the co-founder of Kill Your Darlings Candle Company.</p>
+      <p>As R.R. Campbell, my published novels include <em>Imminent Dawn</em>, which debuted as the number one new release for its genre on Amazon. My titles published as Ryan R. Campbell—including <em>Scambait</em>, which was a finalist in the 2022 International Book Awards for Humor and Satire—are now available with most major retailers.</p>
+      <p>I have been an instructor for the University of Wisconsin Continuing Studies in Writing program and an invited speaker at conferences and seminars including the University of Wisconsin Writers' Institute, Write by the Lake, UntitledTown, Lakefly Writers Conference, WisCon, and Great River Writes. My short-form fiction has been featured in&nbsp;<em>Five:2:One Magazine's #thesideshow</em>, among others.</p>
+      <p>In 2020, I was the judge for the Wisconsin Writers Association's Jade Ring Contest, the state's longest-running prize for fiction.</p>
+      <p>My wife and I live in Milwaukee, where I work as a software engineer for a Fortune 100 financial services and insurance firm.</p>
+      {/* TODO: DO SOMETHING TECH-RELATED HERE? */}
+    </div>
+  </>
+
+export const LongStory = () =>
+  <>
+    <h2>the story (so far)</h2>
+    <div id='on-names'>
+      <h3>call me ishmael</h3>
+      <p>On second thought, don't. I've had enough nicknames for one lifetime already; the list needn't grow longer.</p>
+      <p>That I've had as many nicknames as there are stars in the sky is likely unsurprising, at least not when juxtaposed against the jack-of-all-trades theme that runs throughout this site.</p>
+      <p>And why shouldn't that be the case? The truth is I've been a bit of everything. Pizza-slinging psycholinguist? Check. Soccer-playing polyglot? You know it. Even in my youth, I was somehow both the kid with the lead in the school musical and a member of the state-championship-winning track team.</p>
+      <p>Go figure.</p>
+      <p>Ultimately, it's taken me a long time to understand—and to embrace—who I am, and the only way to understand that for yourself is to see the story in sum, or at least the story as I see it, anyway.</p>
+    </div>
+    <div id='big-plans'>
+      <h3>big plans</h3>
+      <p>The year is 2012, and a bright-eyed boy by the name of, well, it's me. We're talking about me. Anyway, I graduated from college with big plans: I was heading to Brazil for three months to teach English and Spanish, and, once I returned, I planned to continue my independent language studies and apply to MIT's graduate program in psycholinguistics.</p>
+      <p>To think I'd be accepted wasn't unreasonable; I was a high-achieving student who had graduated from UW-Madison with a double major in Spanish and linguistics (with honors in the major!), and my junior- and senior-year hobbies consisted almost entirely of devouring the works of Chomsky, Ladefoged, and Pinker. I'd even written my honors thesis on the very approachable (and not at all unreasonable to bring up casually at dinner parties) topic of the psychological representations of underspecified vowels amongst speakers of languages that feature vowel harmony. Yeehaw!</p>
+      <p>Simply put, a world of opportunity was waiting for me, if only I had the determination to seize it.</p>
+      <p>As I saw it, things were going to work out precisely as planned (definitely! for sure! no doubt about it!), beginning with that three-month stint in Brazil.</p>
+    </div>
+    <div id='southward-bound'>
+      <h3>southward bound</h3>
+      <p>Has this ever happened to you? You head to another country for three months to teach a couple of languages, but then when you get there, it turns out there is no job and you've just moved like 4,000 miles almost entirely for the sake of it? <em>Has this ever happened to you?</em></p>
+      <p>Yeah, it happened to me.</p>
+      <p>I'd spent the previous year preparing for this trip, taking accelerated Portuguese-language courses and setting aside whatever cash I could from my pizza delivery gig to save up for this southbound excursion, but I learned in a matter of days after my arrival that the opportunity I'd been preparing myself for was not, shall we say, as advertised.</p>
+      <p>At this point, I had two options. I could either instantly incinerate what savings I had remaining and move forward my return flight to the U.S.—where I'd presumably be cast into the fires of a good-old-fashioned post-graduation job search—or I could <em>slowly</em> incinerate what savings I had remaining and stay in Brazil, forging friendships and deepening my foreign-language fluency.</p>
+      <p>I should mention that only one of these options also came with diesel-strength espresso.</p>
+      <p>Viewed through that lens, my choice was more or less made for me. Brazil it was!</p>
+      <p>In a surprise to absolutely no one, three months of largely unstructured time—complemented by copious amounts of aforementioned espresso—led to interesting outcomes, the most notable of which was that <b>I finally got around to writing that novel I'd been talking about for years</b>.</p>
+    </div>
+    <div id='the-great-american-novel'>
+      <h3>the great american novel (as written in brazil)</h3>
+      <p>Ah, yes. The Great American Novel. You know, the novel everyone writes when they sit down to write a novel, especially if it's their first. How hard could it be, right?</p>
+      <p>That the novel in question does not figure among <Link to='/books'>my published works</Link> should tell you precisely how hard it in fact is.</p>
+      <p>Naivete aside, over the course of those three months, I did write something vaguely resembling a manuscript, a dream I'd had since I was in elementary school. I thought for sure that once I returned to the U.S., I'd not land my first Real Job™, but I'd also apply to grad school and, naturally, get a novel published on the side.</p>
+      <p>This is not at all what transpired.</p>
+    </div>
+    <div id='best-laid-plans'>
+      <h3>best laid plans</h3>
+      <p>Life comes at you fast, as they say, and I'm not sure life has ever come at me much quicker than in my first few months back stateside.</p>
+      <p>After a fruitless series of interviews in the quest for the fabled Real Job™, I relented and returned to my pizza delivery gig. In between late night pizza delivery shifts, I wrote. Like, <em>all the time</em>. I even carried a back-pocket-sized notepad with me to write on when it wasn't practical to bust out my laptop.</p>
+      <p>I also sought community. Through the University of Wisconsin-Madison's Continuing Studies in Writing program, I enrolled in courses, connected with a critique group, and dedicated myself to attending it religiously every other Saturday morning for years. It turned out I had far more to learn than I ever could have imagined, but I remained determined to see my work through to publication.</p>
+      <p>However, after two years of dedicated practice and study (and countless attempts to attract a literary agent to represent me and my work), I felt no closer to seeing my words in print, and the late-night pizza gig was finally taking its toll. Oh, and that psycholinguistics program at MIT... whatever happened to that?</p>
+      <p>Clearly on the verge of a quarter-life crisis, I did what any reasonable person in my position would do: I quit my job and went to Brazil. <em>Vambora!</em></p>
+    </div>
+    <div id=''>
+      <h3>trade-offs and trade-ups</h3>
+      <p>My second stint in South America coincided perfectly with the 2014 World Cup, which Brazil hosted. After taking a month to rekindle my affinity for radioactive espresso and <em>futebol</em>, I concocted a plan: this time when I returned home, I'd focus not on my dream of becoming an author, but rather on the practicalities of becoming a real life adult who, I deeply hoped, would finally find himself one of those Real Jobs™ everyone kept going on about.</p>
+      <p>This, again, did not go as planned. It seemed that after a couple of additional years in the service industry, employers had me pegged as a lifer. More frustrated than ever, I coped by, you guessed it, returning to writing as much as I could in between job hunting and attempting to up-skill (my first flirtation with JavaScript!).</p>
+      <p>Then, on a late summer day in 2014, fate intervened, by which I mean the HR department of an agricultural firm. To support their sales in Latin America, they were in desperate need of someone who spoke Spanish and Portuguese, which, wouldn't you know it, was something I was more than capable of taking on.</p>
+      <p>Imagine, now, a montage: me, struggling to settle into my first Real Job™ (is this really what everyone does for 40 hours a week until they retire or die?), learning the ins and outs of livestock and feed scales (not to mention the associated vocabulary in Spanish and Portuguese), jumping from the world of agriculture to a similar job in the field of medical devices (and, again, learning the associated vocabulary in Spanish and Portuguese), transitioning from sales support to international regulatory affairs, being offered a publishing contract for a novel—</p>
+      <p>Wait. What? <em>Being offered a publishing contract for a novel?</em></p>
+    </div>
+    <div id=''>
+      <h3>return and rise of all things bookish</h3>
+      <p>Yup, there was no keeping me away from those novels I'd been working on. Despite the daily grind, I'd still been at the hustle: getting up early to write before work, writing on my lunch breaks, writing when I got home, still attending those critique groups on the weekends... the list goes on. It's during this window that I founded <Link to='/podcasts'>the Writescast Network</Link>, which introduced me to hundreds of other authors and industry professionals over its four years in operation.</p>
+      <p>With a publishing contract signed (and a couple of others on their way to my inbox), I left the world of Real Jobs™ behind to finally commit to my ✨<em>Dream Job</em>✨.</p>
+      <p>The following years consisted of book launches, platform-building, speaking gigs, and coaching other authors in their pursuit of publication. For a brief time, I taught through the University of Wisconsin-Madison's Continuing Studies in Writing program and served on the board of the Wisconsin Writers Association. Oh, and my wife and I founded Kill Your Darlings Candle Company, which, as its mission statement would tell you, sustainably illuminated the lives of readers, writers, and their communities.</p>
+      <p>At long last, I was living the life I was meant to be living all along! Everything was exactly as it should have been. Perfect, really. Great. Good. Wonderful. Fine.</p>
+      <p>Why are you making that face? <em>I said everything was <b>fine</b></em>.</p>
+    </div>
+    <div id=''>
+      <h3>everything was fine (except it wasn't)</h3>
+      <p>Like everyone else, I was not immune to the vagaries of the pandemic, during which one personal revelation hit me particularly hard: the path I was on was no longer sustainable, assuming it ever had been at all.</p>
+      <p>In the same way that the pizza delivery gig's late night hours eventually chased me from the service industry, the round-the-clock maintenance of my work as an author, podcaster, writing coach, and business owner had ground my soul into dust, and that's without mention of the gut-wrenching truth—one I'd been ignoring for years—that I had made much more money as a pizza delivery driver than I was ever going to make as an author.</p>
+      <p>More disillusioned than ever, I had to shake things up, and I knew I'd have to try something different. No amount of running away to Brazil was going to save me, not this time.</p>
+    </div>
+    <div>
+      <h3>enter bootcamp</h3>
+      <p>No, not the military kind, the <em>coding</em> kind. In assessing how to align my interests and skill set, I realized my background in linguistics and language learning—along with how much I'd enjoyed building websites for all of my entrepreneurial endeavors in the preceding years—might make me the perfect candidate to take on a new career in web development.</p>
+
+      {/* THIS TIME, THOUGH, IT COULD BE NO MERE FLIRTATION WITH JAVASCRIPT. I KNEW I HAD TO COMMIT, AND I DID. DROPPED EVERYTHING (EXCEPT FOR THE CANDLE COMPANY) FOR SIX MONTHS. LEARNED EVERY LAST THING I COULD. */}
+      {/* THEN TRANSITIONED TO THE JOB HUNT. HANDFUL OF OFFERS, INCLUDING FROM SONY. CHOSE TO MOVE BACK TO THE MKE AREA, THOUGH, TO REALLY START ANEW */}
+      {/* STARTED NEW JOB, SOLD CANDLE COMPANY, PUBLISHED A NOVEL ON MY OWN--FINALLY DOING *ALL THE THINGS* AND EMBRACING THE RIGHT BALANCE IN LIFE */}
+      {/* ALSO USED SKILLS FROM PUBLISHING WORLD IN NEW CAREER: STARTING PODCAST, BRIDGING COMMUNICATION GAPS, SPEAKING AT COMPANY EVENTS AND DURING HACKATHONS */}
+      {/* AFTER A COUPLE OF YEARS FOCUSED ON SETTING MYSELF UP FOR SUCCESS IN A NEW FIELD AND IN A NEW CITY, I'M READY TO FIND THAT BALANCE AGAIN: GETTING BACK INTO WRITING AND EMBRACING WHO I AM AS, WELL, AN "EVERYTHING" GUY */}
+    </div>
+
+    {/* IN THE END, STORY IS ABOUT HOW DESPITE FOLKS SAYING YOU HAVE TO PICK, ETC., YOU'VE ULTIMATELY CHOSEN TO PICK WHATEVER YOU WANT; LIFE IS MORE FUN THAT WAY */}
+  </>
+
