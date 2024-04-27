68052d69aa7cce16624b6e8c5cd4ab765b07b1d3
rrcampbell-exe <campbell.ryan.r@gmail.com>
2024-04-27 10:58:19 -0500

    fix: update mobile styles

--- src/experiences/shared/components/Header/Header.jsx
+++ src/experiences/shared/components/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = ({ pageTitle }) => {
   return (
     <header className='Header'>
       <Link to='/' className='link'>
-        <div>
+        <div className='page-details'>
           <h1>ryan r<span id='header-dot'>.</span> campbell</h1>
           <h2>{pageTitle}</h2>
         </div>
