0eccb65ed86e09bf42d7d043ac82201d146e12ef
rrcampbell-exe <campbell.ryan.r@gmail.com>
2024-05-03 10:52:11 -0500

    chore: update tests

--- src/tests/pages/BlogLanding.test.jsx
+++ src/tests/pages/BlogLanding.test.jsx
@@ -12,7 +12,6 @@ describe('BlogLanding', () => {
     )
     expect(blogLanding).toMatchSnapshot()
   })
-
   test('should render BlogTeaser', () => {
     const { getByTestId } = render(
       <ContextAndRouterProvider>
@@ -22,5 +21,45 @@ describe('BlogLanding', () => {
 
     const blogTeaser = getByTestId('blog-teaser')
     expect(blogTeaser).toBeInTheDocument()
+  })
+  test('should render correct number of posts', () => {
+    const { container } = render(
+      <ContextAndRouterProvider>
+        <BlogLanding />
+      </ContextAndRouterProvider>
+    )
+
+    const posts = container.querySelectorAll('.content-post')
+    expect(posts.length).toBe(9)
+  })
+  test('should load more posts when button is clicked', () => {
+    const { getByText, container } = render(
+      <ContextAndRouterProvider>
+        <BlogLanding />
+      </ContextAndRouterProvider>
+    )
+
+    const button = getByText('load more posts')
+    fireEvent.click(button)
+
+    const posts = container.querySelectorAll('.content-post')
+    expect(posts.length).toBe(18)
+  })
+  test('should show all posts when button is clicked', () => {
+    const { getByText, container } = render(
+      <ContextAndRouterProvider>
+        <BlogLanding />
+      </ContextAndRouterProvider>
+    )
+
+    const button = getByText('load more posts')
+
+    const numberOfClicks = 100
+    for (let i = 0; i < numberOfClicks; i++) {
+      fireEvent.click(button)
+    }
+
+    const updatedButton = getByText('all blog posts shown')
+    expect(updatedButton).toBeInTheDocument()
   })
 })
