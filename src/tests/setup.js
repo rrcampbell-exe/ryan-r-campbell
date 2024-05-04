13809bb8d25378a3350ed8f03a873d6e82c65640
rrcampbell-exe <campbell.ryan.r@gmail.com>
2024-05-04 07:29:05 -0500

    chore: add beforeEach functions

--- src/tests/setup.js
+++ src/tests/setup.js
@@ -1,6 +1,11 @@
-import { afterEach } from 'vitest'
+import { afterEach, beforeEach, vi } from 'vitest'
 import { cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/vitest'
+
+beforeEach(() => {
+  window.scrollTo = vi.fn()
+  window.open = vi.fn()
+})
 
 afterEach(() => {
   cleanup()
