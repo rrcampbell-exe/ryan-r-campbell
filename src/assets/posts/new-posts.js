8a72e136461cf27d62c48091186143021ff374d5
rrcampbell-exe <campbell.ryan.r@gmail.com>
2024-05-10 07:52:46 -0500

    chore: convert filetype, migrate content.rendered

---
+++ src/assets/posts/new-posts.js
@@ -0,0 +1,25 @@
+const posts = [
+  {
+    id: 10088,
+    date: "2024-05-03T06:00:00",
+    slug: "welcome-to-the-new-ryan-r-campbell",
+    link: "https://ryanrcampbell.com/2024/05/03/welcome-to-the-new-ryan-r-campbell",
+    title: {
+      rendered: "Welcome to the New Ryan R. Campbell (Or at Least the New Website)"
+    },
+    content: {
+      rendered:
+        '',
+      protected: false
+    },
+    excerpt: {
+      rendered: "It's been a while—nearly two years, to be exact—but golly, do I have some updates.",
+      protected: false
+    },
+    tags: ['Site Migration', 'Web Development', 'On Writing'],
+    episode_featured_image: "/post-content/2024/05/welcome-to-the-new-site-cover-img.png"
+  }
+];
+
+
+export default posts
