d0fdca2d28c15bf1e22bf3033c12c7d28be3962e
rrcampbell-exe <campbell.ryan.r@gmail.com>
2024-05-22 16:29:29 -0500

    chore: add new blog post

--- src/assets/posts/new-posts.js
+++ src/assets/posts/new-posts.js
@@ -1,5 +1,25 @@
 const posts = [
   {
+    id: 10089,
+    date: "2024-05-23T06:00:00",
+    slug: "book-review-the-good-enough-job",
+    link: "https://ryanrcampbell.com/2024/05/23/book-review-the-good-enough-job",
+    title: {
+      rendered: "Book Review: The Good Enough Job"
+    },
+    content: {
+      rendered:
+        '',
+      protected: false
+    },
+    excerpt: {
+      rendered: "The right book. The right time. Right on.",
+      protected: false
+    },
+    tags: ['Book Review', 'Personal Development', 'Identity'],
+    episode_featured_image: "/post-content/2024/05/welcome-to-the-new-site-cover-img.png"
+  },
+  {
     id: 10088,
     date: "2024-05-03T06:00:00",
     slug: "welcome-to-the-new-ryan-r-campbell",
