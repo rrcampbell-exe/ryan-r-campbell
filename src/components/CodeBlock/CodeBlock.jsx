23cd62644189852a3e147ac321189afa021116e5
rrcampbell-exe <campbell.ryan.r@gmail.com>
2024-05-24 08:28:43 -0500

    feat: add new code blocks, styles for same

--- src/components/CodeBlock/CodeBlock.jsx
+++ src/components/CodeBlock/CodeBlock.jsx
@@ -13,6 +13,32 @@ export const Bash = ({ text, showLineNumbers }) =>
     </div>
   </div>
 
+export const CSS = ({ text, showLineNumbers }) =>
+  <div className='Code-Block' >
+    <div className='css'>
+      <CopyBlock 
+          text={text}
+          language='css'
+          showLineNumbers={showLineNumbers}
+          theme={sunburst}
+          codeBlock
+      />
+    </div>
+  </div>
+
+export const JavaScript = ({ text, showLineNumbers }) =>
+  <div className='Code-Block' >
+    <div className='javascript'>
+      <CopyBlock 
+          text={text}
+          language='javascript'
+          showLineNumbers={showLineNumbers}
+          theme={sunburst}
+          codeBlock
+      />
+    </div>
+  </div>
+
 export const CodeSpan = ({ text }) =>
   <code className='code-span'>{text}</code>
 
