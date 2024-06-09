4e17f8c788c817ea94bc4f583dc692195e90b5c4
rrcampbell-exe <campbell.ryan.r@gmail.com>
2024-06-09 07:44:40 -0500

    fix: restore metadata to index.html

--- src/components/PageWrapper/PageWrapper.jsx
+++ src/components/PageWrapper/PageWrapper.jsx
@@ -4,7 +4,6 @@ import { useLocation } from 'react-router-dom'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/react'
 import { Header, Footer } from '../'
-import { Helmet } from 'react-helmet'
 
 const PageWrapper = ({ pageTitle, children, className }) => {
   const { theme } = useContext(ThemeContext)
@@ -28,25 +27,6 @@ const PageWrapper = ({ pageTitle, children, className }) => {
 
   return (
     <div className={classNameString}>
-      <Helmet>
-        <title>Ryan R. Campbell - Author & Technologist</title>
-        <meta name="description" content="Ryan R. Campbell is a technologist and two-time International Book Awards finalist for his novels Scambait and EMPATHY: Imminent Dawn." />
-        <meta property="image" content="https://www.ryanrcampbell.com/2024-keep-curious-site-cover-img.png" />
-        <meta property="og:title" content="Ryan R. Campbell - Author & Technologist" />
-        <meta property="og:description" content="Ryan R. Campbell is a technologist and two-time International Book Awards finalist for his novels Scambait and EMPATHY: Imminent Dawn." />
-        <meta property="og:image" content="https://www.ryanrcampbell.com/2024-keep-curious-site-cover-img.png" />
-        <meta property="og:site_name" content="Ryan R. Campbell - Author & Technologist" />
-        <meta property="og:url" content="https://www.ryanrcampbell.com" />
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:title" content="Ryan R. Campbell - Author & Technologist" />
-        <meta property="twitter:description" content="Ryan R. Campbell is a technologist and two-time International Book Awards finalist for his novels Scambait and EMPATHY: Imminent Dawn." />
-        <meta property="twitter:image" content="https://www.ryanrcampbell.com/2024-keep-curious-site-cover-img.png"/>
-        <meta property="pinterest:title" content="Ryan R. Campbell - Author & Technologist" />
-        <meta property="pinterest:description" content="Ryan R. Campbell is a technologist and two-time International Book Awards finalist for his novels Scambait and EMPATHY: Imminent Dawn." />
-        <meta property="pinterest:image" content="https://www.ryanrcampbell.com/2024-keep-curious-site-cover-img.png" />
-        <meta property="google:title" content="Ryan R. Campbell - Author & Technologist"/>
-        <meta property="google:description" content="Ryan R. Campbell is a technologist and two-time International Book Awards finalist for his novels Scambait and EMPATHY: Imminent Dawn." />
-      </Helmet>
       <Header pageTitle={pageTitle} />
         {children}
         <div data-testid='analytics'>
