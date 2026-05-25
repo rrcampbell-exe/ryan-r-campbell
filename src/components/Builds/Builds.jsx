4c82e12703fda040b57d7729182a8e27277ff6f4
rrcampbell-exe <campbell.ryan.r@gmail.com>
2026-05-25 10:24:51 -0500

    feat: add real-fake-birds to Builds

--- src/components/Builds/Builds.jsx
+++ src/components/Builds/Builds.jsx
@@ -6,20 +6,39 @@ const Builds = ({ buildsToDisplay }) => {
   return (
     <div className='Builds'>
       {buildsToRender.map((app) => (
-        <div className='build grow-on-hover' key={app.logo} data-testid='build'>
-          <div className='app-overview'>
-            <img className='app-logo' src={app.logo} alt={app.alt} />
-            <h3 className='app-title'>{app.title}</h3>
-            <i className='app-teaser'>{app.teaser}</i>
-            <p>{app.description}</p>
-            <div className='app-overview-underline' />
+        <article className='build-tile' key={app.logo} data-testid='build'>
+          <div className='build-tile__logo-wrap'>
+            <img className='build-tile__logo' src={app.logo} alt={app.alt} />
           </div>
-          <div className='app-details'>
-            <p><b>Technologies used: </b>{app.technologies}</p>
-            <p><a href={app.link} target={app.link && app.link.includes('ryanrcampbell.com') ? '' : '_blank'} rel='noopener'>{app.linkText}</a></p>
-            <p><a href={app.gitHub} target='_blank' rel='noopener noreferrer'>view {app.title.toLowerCase()} on github</a></p>
+          <div className='build-tile__body'>
+            <h3 className='build-tile__title'>{app.title}</h3>
+            <p className='build-tile__teaser'>{app.teaser}</p>
+            <p className='build-tile__desc'>{app.description}</p>
+            <p className='build-tile__tech'>{app.technologies}</p>
           </div>
-        </div>
+          <div className='build-tile__links'>
+            {app.link && (
+              <a
+                href={app.link}
+                target={app.link.includes('ryanrcampbell.com') ? '' : '_blank'}
+                rel='noopener noreferrer'
+                className='build-tile__link'
+              >
+                {app.linkText}
+              </a>
+            )}
+            {app.gitHub && (
+              <a
+                href={app.gitHub}
+                target='_blank'
+                rel='noopener noreferrer'
+                className='build-tile__link build-tile__link--gh'
+              >
+                GitHub
+              </a>
+            )}
+          </div>
+        </article>
       ))}
     </div>
   )
